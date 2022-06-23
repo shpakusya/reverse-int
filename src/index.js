0cf3a0c1248ff9c8e8cdcfdde3d71b902d5c4aa8
Kate Shpakovskaya <kate.shpakovskaya02@gmail.com>
2022-06-23 13:23:36 +0300

    fix: delete operational log

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ module.exports = function reverse (n) {
     let stringarr = nmod.toString().split('');
     let reversedarr = stringarr.reverse();
     let reversed='';
-    console.log(reversedarr)
     for(let i = 0; i<reversedarr.length;i++){
       reversed+=Number(reversedarr[i]);
     }
