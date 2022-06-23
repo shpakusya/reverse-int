4e16465c6b6b49c5ed1a2ff07cc0032d78f06b92
Kate Shpakovskaya <kate.shpakovskaya02@gmail.com>
2022-06-23 13:21:54 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let nmod = Math.abs(n);
+    let stringarr = nmod.toString().split('');
+    let reversedarr = stringarr.reverse();
+    let reversed='';
+    console.log(reversedarr)
+    for(let i = 0; i<reversedarr.length;i++){
+      reversed+=Number(reversedarr[i]);
+    }
+    return reversed;
 }
